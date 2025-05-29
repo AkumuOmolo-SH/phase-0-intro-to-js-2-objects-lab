029d1feab37932b3bceefd53e731a13977f4f20e
AkumuOmolo <akumu.omoloh@gmail.com>
2025-05-29 19:12:50 +0300

    Adds index.js package-lock.json

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee =  { 
+    name: "John",
+    streetAddress: "80 Dam Estate"
+
+}
+
+const updatedEmployee = updateEmployeeWithKeyAndValue(employee, "streetAddress", "80 Dam Estate");
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return{
+        ...obj, 
+        [key]: value,
+    };
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee };
+    delete newEmployee[key];
+    return newEmployee;
+
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+
+}
